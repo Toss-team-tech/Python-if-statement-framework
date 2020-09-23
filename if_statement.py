162325ef0cb72dcfde5abf248d2f31bc7fd33361
Toss-team-tech <71771492+Toss-team-tech@users.noreply.github.com>
2020-09-23 19:10:55 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,9 @@
+number1 = input('enter the first number ')
+number2 = input('enter the second number')
+
+if number1 > number2:
+    print(number1+' is greater than 5 '+number2)
+elif number1 < number2:
+    print(number2+' is greater than  '+number1)
+else:
+    print('Both are equal')
